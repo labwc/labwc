labwc/labwc

commit f857aea8f4c86cdff0c6c2e5687cb129af0ad325

ssd: add application title to title bar

src/output.c
@@ -488,7 +488,7 @@ render_deco(struct view *view, struct output *output,
 				*(part->texture.inactive);
 			render_texture_helper(output, output_damage, &part->box,
 					      texture);
-		} else {
+		} else if (part->color.active && part->color.inactive) {
 			float *color = focused ?
 				part->color.active :
 				part->color.inactive;

include/labwc.h
@@ -222,6 +222,7 @@ struct view {
 		struct wl_list parts;
 		struct wlr_box box; /* remember geo so we know when to update */
 	} ssd;
+	struct wlr_texture *title;
 
 	struct wlr_foreign_toplevel_handle_v1 *toplevel_handle;
 	struct wl_listener toplevel_handle_request_maximize;

include/ssd.h
@@ -11,6 +11,7 @@ enum ssd_part_type {
 	LAB_SSD_BUTTON_CLOSE,
 	LAB_SSD_BUTTON_MAXIMIZE,
 	LAB_SSD_BUTTON_ICONIFY,
+	LAB_SSD_PART_TITLEBAR,
 	LAB_SSD_PART_TITLE,
 	LAB_SSD_PART_CORNER_TOP_LEFT,
 	LAB_SSD_PART_CORNER_TOP_RIGHT,
@@ -56,6 +57,7 @@ struct wlr_box ssd_max_extents(struct view *view);
 struct wlr_box ssd_box(struct view *view, enum ssd_part_type type);
 enum ssd_part_type ssd_at(struct view *view, double lx, double ly);
 uint32_t ssd_resize_edges(enum ssd_part_type type);
+void ssd_update_title(struct view *view);
 void ssd_create(struct view *view);
 void ssd_destroy(struct view *view);
 void ssd_update_geometry(struct view *view);

src/cursor.c
@@ -321,7 +321,7 @@ cursor_button(struct wl_listener *listener, void *data)
 	desktop_focus_view(&server->seat, view);
 	damage_all_outputs(server);
 
-	if (is_double_click(500) && view_area == LAB_SSD_PART_TITLE) {
+	if (is_double_click(500) && view_area == LAB_SSD_PART_TITLEBAR) {
 		view_toggle_maximize(view);
 		return;
 	}
@@ -339,7 +339,7 @@ cursor_button(struct wl_listener *listener, void *data)
 	case LAB_SSD_BUTTON_ICONIFY:
 		view_minimize(view, true);
 		break;
-	case LAB_SSD_PART_TITLE:
+	case LAB_SSD_PART_TITLEBAR:
 		interactive_begin(view, LAB_INPUT_STATE_MOVE, 0);
 		break;
 	case LAB_SSD_BUTTON_MAXIMIZE:

src/foreign.c
@@ -23,6 +23,12 @@ foreign_toplevel_handle_create(struct view *view)
 {
 	view->toplevel_handle = wlr_foreign_toplevel_handle_v1_create(
 		view->server->foreign_toplevel_manager);
+	if (!view->toplevel_handle) {
+		wlr_log(WLR_ERROR, "cannot create foreign toplevel handle for (%s)",
+			view->impl->get_string_prop(view, "title"));
+		return;
+	}
+
 	view_update_title(view);
 	wlr_foreign_toplevel_handle_v1_output_enter(view->toplevel_handle,
 		view_wlr_output(view));
@@ -35,4 +41,5 @@ foreign_toplevel_handle_create(struct view *view)
 		handle_toplevel_handle_request_minimize;
 	wl_signal_add(&view->toplevel_handle->events.request_minimize,
 		&view->toplevel_handle_request_minimize);
+	// TODO: hook up remaining signals
 }

src/ssd.c
@@ -6,6 +6,7 @@
 
 #include <assert.h>
 #include "config/rcxml.h"
+#include "common/font.h"
 #include "labwc.h"
 #include "theme.h"
 #include "ssd.h"
@@ -63,7 +64,7 @@ ssd_interactive_box(struct view *view, enum ssd_part_type type)
 		box.width = theme->title_height;
 		box.height = theme->title_height;
 		break;
-	case LAB_SSD_PART_TITLE:
+	case LAB_SSD_PART_TITLEBAR:
 		box.x = view->x;
 		box.y = view->y - theme->title_height;
 		box.width = view->w;
@@ -138,10 +139,19 @@ ssd_box(struct view *view, enum ssd_part_type type)
 	case LAB_SSD_BUTTON_ICONIFY:
 		box = ssd_interactive_box(view, type);
 		break;
-	case LAB_SSD_PART_TITLE:
+	case LAB_SSD_PART_TITLEBAR:
 		box = ssd_interactive_box(view, type);
 		box.x += theme->title_height;
 		box.width -= 2 * theme->title_height;
+		break;
+	case LAB_SSD_PART_TITLE:
+		box = ssd_box(view, LAB_SSD_PART_TITLEBAR);
+		if (view->title) {
+			/* center align title vertically within allocated box */
+			box.y += (box.height - view->title->height) / 2;
+			box.width = view->title->width;
+			box.height = view->title->height;
+		}
 		break;
 	case LAB_SSD_PART_CORNER_TOP_LEFT:
 		box = ssd_interactive_box(view, type);
@@ -232,6 +242,27 @@ add_part(struct view *view, enum ssd_part_type type)
 }
 
 void
+ssd_update_title(struct view *view)
+{
+	struct theme *theme = view->server->theme;
+
+	/* TODO: use window.active.label.text.color here */
+	/* TODO: set max_width propertly */
+	font_texture_create(view->server, &view->title, 200,
+		view->impl->get_string_prop(view, "title"),
+		rc.font_name_activewindow,
+		theme->menu_items_active_text_color);
+
+	struct ssd_part *part;
+	wl_list_for_each(part, &view->ssd.parts, link) {
+		if (part->type == LAB_SSD_PART_TITLE) {
+			part->box = ssd_box(view, part->type);
+			break;
+		}
+	}
+}
+
+void
 ssd_create(struct view *view)
 {
 	struct theme *theme = view->server->theme;
@@ -257,10 +288,16 @@ ssd_create(struct view *view)
 	}
 
 	/* titlebar */
-	part = add_part(view, LAB_SSD_PART_TITLE);
-	part->box = ssd_box(view, LAB_SSD_PART_TITLE);
+	part = add_part(view, LAB_SSD_PART_TITLEBAR);
+	part->box = ssd_box(view, LAB_SSD_PART_TITLEBAR);
 	part->color.active = theme->window_active_title_bg_color;
 	part->color.inactive = theme->window_inactive_title_bg_color;
+
+	/* title text */
+	part = add_part(view, LAB_SSD_PART_TITLE);
+	ssd_update_title(view);
+	part->texture.active = &view->title;
+	part->texture.inactive = &view->title;
 
 	/* titlebar top-left corner */
 	part = add_part(view, LAB_SSD_PART_CORNER_TOP_LEFT);

src/view.c
@@ -173,5 +173,6 @@ view_update_title(struct view *view)
 	if (!view->toplevel_handle || !title) {
 		return;
 	}
+	ssd_update_title(view);
 	wlr_foreign_toplevel_handle_v1_set_title(view->toplevel_handle, title);
 }

commit 9a9cd609f667ae4e29509ee3ddf3678839426581

menu: fix breakage caused by f857aea8

include/menu/menu.h
@@ -15,17 +15,15 @@ struct menuitem {
 		int offset_y;
 	} texture;
 	bool selected;
-	struct wl_list link;
+	struct wl_list link; /* menu::menuitems */
 };
 
 struct menu {
 	struct server *server;
-	int x;
-	int y;
+	struct wlr_box box;
 	struct wl_list menuitems;
 };
 
-/* menu_create - create menu */
 void menu_init_rootmenu(struct server *server, struct menu *menu);
 void menu_finish(struct menu *menu);
 

src/menu/menu.c
@@ -231,16 +231,19 @@ menu_finish(struct menu *menu)
 void
 menu_move(struct menu *menu, int x, int y)
 {
-	menu->x = x;
-	menu->y = y;
+	menu->box.x = x;
+	menu->box.y = y;
 
 	int offset = 0;
 	struct menuitem *menuitem;
 	wl_list_for_each_reverse (menuitem, &menu->menuitems, link) {
-		menuitem->box.x = menu->x;
-		menuitem->box.y = menu->y + offset;
+		menuitem->box.x = menu->box.x;
+		menuitem->box.y = menu->box.y + offset;
 		offset += menuitem->box.height;
 	}
+
+	menu->box.width = MENUWIDTH;
+	menu->box.height = offset;
 }
 
 void

src/output.c
@@ -535,28 +535,39 @@ static void
 render_rootmenu(struct output *output, pixman_region32_t *output_damage)
 {
 	struct server *server = output->server;
+	struct theme *theme = server->theme;
 	float matrix[9];
 
 	struct wlr_output_layout *output_layout = server->output_layout;
 	double ox = 0, oy = 0;
 	wlr_output_layout_output_coords(output_layout, output->wlr_output,
 		&ox, &oy);
+
+	/* background */
+	render_rect(output, output_damage, &server->rootmenu->box,
+		theme->menu_items_bg_color);
+
+	/* items */
 	struct menuitem *menuitem;
 	wl_list_for_each (menuitem, &server->rootmenu->menuitems, link) {
-		struct wlr_texture *t;
-		t = menuitem->selected ? menuitem->texture.active :
-			menuitem->texture.inactive;
 		struct wlr_box box = {
-			.x = menuitem->box.x + ox + menuitem->texture.offset_x,
-			.y = menuitem->box.y + oy + menuitem->texture.offset_y,
-			.width = t->width,
-			.height = t->height,
+			.x = menuitem->box.x + menuitem->texture.offset_x + ox,
+			.y = menuitem->box.y + menuitem->texture.offset_y + oy,
+			.width = menuitem->texture.active->width,
+			.height = menuitem->texture.active->height,
 		};
 		scale_box(&box, output->wlr_output->scale);
 		wlr_matrix_project_box(matrix, &box, WL_OUTPUT_TRANSFORM_NORMAL,
 			0, output->wlr_output->transform_matrix);
-		render_texture(output->wlr_output, output_damage, t,
-			&box, matrix);
+		if (menuitem->selected) {
+			render_rect(output, output_damage, &menuitem->box,
+				theme->menu_items_active_bg_color);
+			render_texture(output->wlr_output, output_damage,
+				menuitem->texture.active, &box, matrix);
+		} else {
+			render_texture(output->wlr_output, output_damage,
+				menuitem->texture.inactive, &box, matrix);
+		}
 	}
 }
 
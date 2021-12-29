c797dbe189902667022e779b8358af71f116369f
Johan Malm <jgm323@gmail.com>
2021-12-29 14:04:41 +0000

    config: de-couple <FollowMouse> and <raiseOnFocus>
    Set these two variables indepently of each other as this is cleaner and
    more explicit.
    
    In openbox <raiseOnFocus> only applies if <followMouse> is set, but there
    is no need to immitate this.

--- src/config/rcxml.c
+++ src/config/rcxml.c
@@ -335,9 +335,6 @@ entry(xmlNode *node, char *nodename, char *content)
 		rc.focus_follow_mouse = get_bool(content);
 	} else if (!strcasecmp(nodename, "raiseOnFocus.focus")) {
 		rc.raise_on_focus = get_bool(content);
-		if (rc.raise_on_focus) {
-			rc.focus_follow_mouse = true;
-		}
 	} else if (!strcasecmp(nodename, "doubleClickTime.mouse")) {
 		long doubleclick_time_parsed = strtol(content, NULL, 10);
 		if (doubleclick_time_parsed > 0) {

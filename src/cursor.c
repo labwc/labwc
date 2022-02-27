labwc/labwc

commit ce38d2dbd6cff723bca12bb423a569bae8354bfb

cursor: add cursor_update_focus()
...and call it from desktop_move_to_front() in order force an enter event
on the surface below the cursor when cycling views.

Fixes #162 and #225
Inspired by PR #164 - just restructured it a bit.

Suggested-by: @bi4k8
Co-authored-by: Consolatis <35009135+Consolatis@users.noreply.github.com>

src/cursor.c
@@ -325,6 +325,20 @@ process_cursor_motion(struct server *server, uint32_t time)
 	}
 }
 
+static uint32_t
+msec(const struct timespec *t)
+{
+	return t->tv_sec * 1000 + t->tv_nsec / 1000000;
+}
+
+void
+cursor_update_focus(struct server *server)
+{
+	struct timespec now;
+	clock_gettime(CLOCK_MONOTONIC, &now);
+	cursor_rebase(&server->seat, msec(&now));
+}
+
 void
 start_drag(struct wl_listener *listener, void *data)
 {
@@ -754,7 +768,6 @@ cursor_axis(struct wl_listener *listener, void *data)
 	wlr_idle_notify_activity(seat->wlr_idle, seat->seat);
 
 	/* Notify the client with pointer focus of the axis event. */
-	cursor_rebase(seat, event->time_msec);
 	wlr_seat_pointer_notify_axis(seat->seat, event->time_msec,
 		event->orientation, event->delta, event->delta_discrete,
 		event->source);

include/labwc.h
@@ -479,6 +479,14 @@ void cursor_rebase(struct seat *seat, uint32_t time_msec);
  */
 void cursor_set(struct seat *seat, const char *cursor_name);
 
+/**
+ * cursor_update_focus - update cursor focus
+ * @server - server
+ * Use it to force an update of the cursor icon and to send an enter event
+ * to the surface below the cursor.
+ */
+void cursor_update_focus(struct server *server);
+
 void cursor_init(struct seat *seat);
 void cursor_finish(struct seat *seat);
 

src/desktop.c
@@ -61,6 +61,7 @@ desktop_move_to_front(struct view *view)
 #if HAVE_XWAYLAND
 	move_xwayland_sub_views_to_front(view);
 #endif
+	cursor_update_focus(view->server);
 }
 
 static void

commit ce6e3de7b6718706954a64169717ac734171d98e

do not clear/reset focus on the same surface
this regressed in ce38d2d

the previous behavior broke double-click selection in the foot terminal emulator

src/cursor.c
@@ -23,6 +23,16 @@ cursor_rebase(struct seat *seat, uint32_t time_msec)
 		seat->cursor->y, &surface, &sx, &sy, &view_area);
 
 	if (surface) {
+		struct view *focused_view = desktop_focused_view(seat->server);
+		struct wlr_surface* focused_surface = focused_view
+			? focused_view->surface
+			: NULL;
+
+		/* do not notify unless rebasing changes which surface has focus */
+		if (surface == focused_surface) {
+			return;
+		}
+
 		wlr_seat_pointer_notify_clear_focus(seat->seat);
 		wlr_seat_pointer_notify_enter(seat->seat, surface, sx, sy);
 		wlr_seat_pointer_notify_motion(seat->seat, time_msec, sx, sy);
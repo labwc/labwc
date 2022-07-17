6c2bbb42ea612831da826cd80fdf71e8ec7c6079
Joshua Ashton <joshua@froggi.es>
2022-07-17 00:47:20 +0000

    common: Add helpers for raising/lowering FD limit
    Signed-off-by: Joshua Ashton <joshua@froggi.es>

---
+++ include/common/fd_util.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+#ifndef __LABWC_FD_UTIL_H
+#define __LABWC_FD_UTIL_H
+
+void increase_nofile_limit(void);
+void restore_nofile_limit(void);
+
+#endif /* __LABWC_FD_UTIL_H */

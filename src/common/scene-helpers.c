369c84c0c6ab2241277f0a4615e9518c1991a3e1
Consolatis <35009135+Consolatis@users.noreply.github.com>
2023-01-30 05:30:24 +0100

    Restore nested resize
    Chases: 756ecf8ee9f1e75bc7b8297dc84f97c7d699174b
    backend/wayland: use request_state when toplevel is resized
    
    Chases: 3ef68a484243555b020200c6f95246d994932c3f
    backend/x11: use request_state when window is resized
    
    Ref: https://gitlab.freedesktop.org/wlroots/wlroots/-/merge_requests/2693
    
    We now delay requested resolution changes by the backend until
    the next frame event which causes us to render the new content
    on the already enlarged buffer. Before this change, an empty
    (black) buffer would have been shown instead before the next
    frame event caused a new render of the actual contents.
    
    Keep commiting the new state and then scheduling a frame event
    would not help as due to the commit call it would still show an
    empty buffer in the meantime.
    
    Just modifying wlr_output->pending wouldn't work either because
    wlr_scene_output_commit() *completely* ignores it (and it will
    be removed in future wlroots commits). For this reason we move
    to wlr_scene_output_build_state() directly because it allows us
    to supply the current wlr_output->pending state and thus apply
    any resolution change in lockstep with new rendering. Result:
    No more flickering in the wayland backend and resizing is again
    smooth as butter.
    
    This prevents constant flicker while resizing
    when running nested via the wayland backend.
    
    For the X11 backend (can be tested via `WLR_BACKENDS=x11 labwc`),
    it is still rather janky but at least doesn't cause endless self-
    resizing anymore.

--- src/common/scene-helpers.c
+++ src/common/scene-helpers.c
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: GPL-2.0-only
 
 #include <assert.h>
+#include <wlr/types/wlr_output.h>
 #include <wlr/types/wlr_scene.h>
+#include <wlr/util/log.h>
 #include "common/scene-helpers.h"
 
 struct wlr_scene_rect *
@@ -44,4 +46,39 @@ lab_wlr_scene_get_prev_node(struct wlr_scene_node *node)
 		return NULL;
 	}
 	return prev;
+}
+
+/*
+ * This is a copy of wlr_scene_output_commit()
+ * as it doesn't use the pending state at all.
+ */
+bool
+lab_wlr_scene_output_commit(struct wlr_scene_output *scene_output)
+{
+	assert(scene_output);
+	struct wlr_output *wlr_output = scene_output->output;
+	struct wlr_output_state *state = &wlr_output->pending;
+
+	if (!wlr_output->needs_frame && !pixman_region32_not_empty(
+			&scene_output->damage_ring.current)) {
+		return false;
+	}
+	if (!wlr_scene_output_build_state(scene_output, state, NULL)) {
+		wlr_log(WLR_ERROR, "Failed to build output state for %s",
+			wlr_output->name);
+		return false;
+	}
+	if (!wlr_output_commit(wlr_output)) {
+		wlr_log(WLR_ERROR, "Failed to commit output %s",
+			wlr_output->name);
+		return false;
+	}
+	/*
+	 * FIXME: Remove the following line as soon as
+	 * https://gitlab.freedesktop.org/wlroots/wlroots/-/merge_requests/4253
+	 * is merged. At that point wlr_scene handles damage tracking internally
+	 * again.
+	 */
+	wlr_damage_ring_rotate(&scene_output->damage_ring);
+	return true;
 }

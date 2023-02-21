216056a6536469f6f1a04d33b2cf3faaa867a818
John Lindgren <john@jlindgren.net>
2023-02-21 08:03:17 -0500

    Chase wlroots: Add missing #include
    Chases: 323f0b94db22877487bca073fca497cd033719e8
    wlr_scene: Cleanup header includes

--- src/common/scaled_scene_buffer.c
+++ src/common/scaled_scene_buffer.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <wayland-server-core.h>
 #include <wlr/types/wlr_buffer.h>
+#include <wlr/types/wlr_output.h>
 #include <wlr/types/wlr_scene.h>
 #include <wlr/util/log.h>
 #include "buffer.h"

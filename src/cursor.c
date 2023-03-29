labwc/labwc

commit 4dc99e2f3856650f3b61778f015a823cfae978d2

cursor: update cursor image on scale change

src/cursor.c
@@ -259,6 +259,17 @@ cursor_set(struct seat *seat, enum lab_cursors cursor)
 	seat->server_cursor = cursor;
 }
 
+void
+cursor_update_image(struct seat *seat)
+{
+	enum lab_cursors cursor = seat->server_cursor;
+	if (cursor == LAB_CURSOR_CLIENT) {
+		return;
+	}
+	wlr_xcursor_manager_set_cursor_image(
+		seat->xcursor_manager, cursor_names[cursor], seat->cursor);
+}
+
 bool
 input_inhibit_blocks_surface(struct seat *seat, struct wl_resource *resource)
 {

include/cursor.h
@@ -106,6 +106,16 @@ enum lab_cursors cursor_get_from_edge(uint32_t resize_edges);
  */
 void cursor_update_focus(struct server *server);
 
+/**
+ * cursor_update_image - re-set the labwc cursor image
+ * @seat - seat
+ *
+ * This can be used to update the cursor image on output scale changes.
+ * If the current cursor image was not set by labwc but some client
+ * this is a no-op.
+ */
+void cursor_update_image(struct seat *seat);
+
 void cursor_init(struct seat *seat);
 void cursor_finish(struct seat *seat);
 

src/output.c
@@ -264,15 +264,11 @@ output_update_for_layout_change(struct server *server)
 
 	/*
 	 * "Move" each wlr_output_cursor (in per-output coordinates) to
-	 * align with the seat cursor. Set a default cursor image so
-	 * that the cursor isn't invisible on new outputs.
-	 *
-	 * TODO: remember the most recent cursor image (see cursor.c)
-	 * and set that rather than XCURSOR_DEFAULT
+	 * align with the seat cursor. Re-set the cursor image so that
+	 * the cursor isn't invisible on new outputs.
 	 */
 	wlr_cursor_move(server->seat.cursor, NULL, 0, 0);
-	wlr_xcursor_manager_set_cursor_image(server->seat.xcursor_manager,
-		XCURSOR_DEFAULT, server->seat.cursor);
+	cursor_update_image(&server->seat);
 }
 
 static void
@@ -364,6 +360,7 @@ handle_output_manager_apply(struct wl_listener *listener, void *data)
 
 	/* Re-set cursor image in case scale changed */
 	cursor_update_focus(server);
+	cursor_update_image(&server->seat);
 }
 
 /*

commit de315127416a04a71ec82276d54a8df868d36773

src/cursor.c: fix invisible cursor on output loss / restore
Previously, the cursor image was only updated on output loss when the
cursor was on a labwc owned surface. This patch forces a re-enter of a
client surface in the remaining case of cursor being over a non-labwc
surface which causes the client to re-set its own cursor image.

This fixes a regression caused by 4dc99e2f3856650f3b61778f015a823cfae978d2.
Thanks to @Flrian for finding the root cause of the issue.

Fixes #820

Reported-by: @Flrian
Tested-by: @Flrian

src/cursor.c
@@ -260,6 +260,16 @@ cursor_update_image(struct seat *seat)
 {
 	enum lab_cursors cursor = seat->server_cursor;
 	if (cursor == LAB_CURSOR_CLIENT) {
+		/*
+		 * When we loose the output cursor while over a client
+		 * surface (e.g. output was destroyed and we now deal with
+		 * a new output instance), we have to force a re-enter of
+		 * the surface so the client sets its own cursor again.
+		 */
+		if (seat->seat->pointer_state.focused_surface) {
+			seat->server_cursor = LAB_CURSOR_DEFAULT;
+			cursor_update_focus(seat->server);
+		}
 		return;
 	}
 	wlr_xcursor_manager_set_cursor_image(
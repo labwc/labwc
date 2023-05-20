78aec7945c093b6ec2c643448630cfb77305f27e
Johan Malm <jgm323@gmail.com>
2023-05-20 08:58:42 +0100

    xdg-popup: render above always-on-top

--- include/labwc.h
+++ include/labwc.h
@@ -254,7 +254,14 @@ struct server {
 
 	/* Tree for all non-layer xdg/xwayland-shell surfaces */
 	struct wlr_scene_tree *view_tree;
-	/* Tree for all non-layer xdg/xwayland-shell surfaces with always-on-top */
+
+	/*
+	 * Popups need to be rendered above always-on-top views, so we reparent
+	 * them to this dedicated tree
+	 */
+	struct wlr_scene_tree *xdg_popup_tree;
+
+	/* Tree for all non-layer xdg/xwayland-shell surfaces with always-on-top/below */
 	struct wlr_scene_tree *view_tree_always_on_top;
 #if HAVE_XWAYLAND
 	/* Tree for unmanaged xsurfaces without initialized view (usually popups) */

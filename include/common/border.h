3a9dcd54a74b10d12884cfcf65e85124dce3202f
Johan Malm <jgm323@gmail.com>
2023-06-05 21:48:47 +0100

    Move border-struct to common/border.h for sharing

---
+++ include/common/border.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+#ifndef LABWC_BORDER_H
+#define LABWC_BORDER_H
+
+struct border {
+	int top;
+	int right;
+	int bottom;
+	int left;
+};
+
+#endif /* LABWC_BORDER_H */

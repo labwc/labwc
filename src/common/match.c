c08fa3249eae7165f7b1354b13d93de5063ccf66
Consus <consus@ftml.net>
2023-10-01 14:39:47 +0300

    common: use fnmatch() for pattern matching
    Drop-in POSIX-compliant function that has a nice glob(7) manual page for
    reference.

--- src/common/match.c
+++ src/common/match.c
@@ -1,15 +1,10 @@
 // SPDX-License-Identifier: GPL-2.0-only
-#define _POSIX_C_SOURCE 200809L
+
+#include <fnmatch.h>
 #include "common/match.h"
 
 bool
-match_glob(const gchar *pattern, const gchar *string)
+match_glob(const char *pattern, const char *string)
 {
-	gchar *p = g_utf8_casefold(pattern, -1);
-	gchar *s = g_utf8_casefold(string, -1);
-	bool ret = g_pattern_match_simple(p, s);
-	g_free(p);
-	g_free(s);
-	return ret;
+	return fnmatch(pattern, string, FNM_CASEFOLD) == 0;
 }
-

2888dbf597a77224e623fd7ddabd2088738e769c
Johan Malm <jgm323@gmail.com>
2023-10-10 21:51:58 +0100

    layers: arrange layers on destroy
    ...rather than unmap because that is the approach of sway and it also
    avoids wshowkeys crashing.
    
    Fixes: #1153

--- include/layers.h
+++ include/layers.h
@@ -10,6 +10,8 @@ struct output;
 struct lab_layer_surface {
 	struct wlr_scene_layer_surface_v1 *scene_layer_surface;
 	struct server *server;
+	struct output *output;
+	struct wlr_scene_tree *tree;
 
 	bool mapped;
 

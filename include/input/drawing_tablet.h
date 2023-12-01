5e79acd14a158279d606e1406509809e67a5c1dd
Jens Peters <jp7677@gmail.com>
2023-12-01 22:12:00 +0100

    input: add drawing tablet setup and event listeners
    Co-authored-by: Consolatis <35009135+Consolatis@users.noreply.github.com>

---
+++ include/input/drawing_tablet.h
@@ -0,0 +1,25 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+#ifndef LABWC_DRAWING_TABLET_H
+#define LABWC_DRAWING_TABLET_H
+
+#include <wayland-server-core.h>
+struct seat;
+struct wlr_device;
+struct wlr_input_device;
+
+struct drawing_tablet {
+	struct seat *seat;
+	struct wlr_tablet *tablet;
+	double x, y;
+	struct {
+		struct wl_listener axis;
+		struct wl_listener tip;
+		struct wl_listener button;
+		struct wl_listener destroy;
+		// no interest in proximity events
+	} handlers;
+};
+
+void drawing_tablet_setup_handlers(struct seat *seat, struct wlr_input_device *wlr_input_device);
+
+#endif /* LABWC_DRAWING_TABLET_H */

ae83a688b1a070c2c749849ad238677c4cd83387
Consolatis <35009135+Consolatis@users.noreply.github.com>
2023-12-17 02:16:49 +0100

    theme: create hover button fallbacks
    ...by copying the non-hover variant and adding a transparent overlay.
    
    Co-authored-by: @johanmalm

--- include/common/string-helpers.h
+++ include/common/string-helpers.h
@@ -3,6 +3,15 @@
 #define LABWC_STRING_HELPERS_H
 
 /**
+ * trim_last_field() - Trim last field of string splitting on provided delim
+ * @buf: string to trim
+ * @delim: delimitator
+ *
+ * Example: With delim='_' and buf="foo_bar_baz" the return value is "foo_bar"
+ */
+void trim_last_field(char *buf, char delim);
+
+/**
  * string_strip - strip white space left and right
  * Note: this function does a left skip, so the returning pointer cannot be
  * used to free any allocated memory

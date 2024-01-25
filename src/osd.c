c71fccbcec7749635bac1967942fe18d1873f8f8
Johan Malm <jgm323@gmail.com>
2024-01-25 19:54:59 +0000

    osd: include always-on-top views in window-switcher

--- src/osd.c
+++ src/osd.c
@@ -410,7 +410,6 @@ display_osd(struct output *output)
 	wl_array_init(&views);
 	view_array_append(server, &views,
 		LAB_VIEW_CRITERIA_CURRENT_WORKSPACE
-		| LAB_VIEW_CRITERIA_NO_ALWAYS_ON_TOP
 		| LAB_VIEW_CRITERIA_NO_SKIP_WINDOW_SWITCHER);
 
 	float scale = output->wlr_output->scale;

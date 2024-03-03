c9d08f82184a82d5c71ce79f069a4d4707528578
Andrew J. Hesford <ajh@sideband.org>
2024-03-03 15:04:24 -0500

    session: only update activation environment...
    ...when running DRM backend or by explicit request

--- include/config/session.h
+++ include/config/session.h
@@ -2,6 +2,8 @@
 #ifndef LABWC_SESSION_H
 #define LABWC_SESSION_H
 
+struct server;
+
 /**
  * session_environment_init - set enrivonment variables based on <key>=<value>
  * pairs in `${XDG_CONFIG_DIRS:-/etc/xdg}/lawbc/environment` with user override
@@ -13,12 +15,12 @@ void session_environment_init(void);
  * session_autostart_init - run autostart file as shell script
  * Note: Same as `sh ~/.config/labwc/autostart` (or equivalent XDG config dir)
  */
-void session_autostart_init(void);
+void session_autostart_init(struct server *server);
 
 /**
  * session_shutdown - run session shutdown file as shell script
  * Note: Same as `sh ~/.config/labwc/shutdown` (or equivalent XDG config dir)
  */
-void session_shutdown(void);
+void session_shutdown(struct server *server);
 
 #endif /* LABWC_SESSION_H */

bd4d92bad86f0a2bbac69c645df54955883b45b1
SnowNF <SnowNF@outlook.com>
2024-03-20 03:32:41 +0800

    libinput: add support for touchscreen set matrix
    My android pad emit a strange touch input from libinput, need to rotate it in 270 degree clockwise to fix.

--- src/config/rcxml.c
+++ src/config/rcxml.c
@@ -611,6 +611,12 @@ fill_libinput_category(char *nodename, char *content)
 	} else if (!strcasecmp(nodename, "sendEventsMode")) {
 		current_libinput_category->send_events_mode =
 			get_send_events_mode(content);
+	} else if (!strcasecmp(nodename, "calibrationMatrix")) {
+		float *m = current_libinput_category->calibration_matrix;
+		int r = sscanf(content, "%f%f%f%f%f%f", &m[0], &m[1], &m[2], &m[3], &m[4], &m[5]);
+		if (r == 6) {
+			current_libinput_category->no_calibration_matrix = false;
+		}
 	}
 }
 

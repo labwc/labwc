42095407969edd1f3c68fd91379fd582f8f82ed5
tokyo4j <hrak1529@gmail.com>
2024-03-24 22:24:40 +0900

    [fixup] remove `overlay.pending` and add `overlay.active.output`

--- src/input/cursor.c
+++ src/input/cursor.c
@@ -225,7 +225,7 @@ process_cursor_move(struct server *server, uint32_t time)
 	resistance_move_apply(view, &dx, &dy);
 	view_move(view, dx, dy);
 
-	overlay_show(&server->seat, view);
+	overlay_show(&server->seat);
 }
 
 static void

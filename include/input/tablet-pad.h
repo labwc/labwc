3c0cea92f26557103285a716ef7fd6ee5807b925
Jens Peters <jp7677@gmail.com>
2024-06-04 20:21:12 +0200

    input: support tablet-v2 pad notifications

--- include/input/tablet-pad.h
+++ include/input/tablet-pad.h
@@ -3,6 +3,7 @@
 #define LABWC_TABLET_PAD_H
 
 #include <wayland-server-core.h>
+#include <wlr/types/wlr_tablet_v2.h>
 
 struct seat;
 struct wlr_device;
@@ -19,15 +20,24 @@ struct wlr_input_device;
 #define LAB_BTN_PAD9 0x8
 
 struct drawing_tablet_pad {
+	struct wlr_input_device *wlr_input_device;
 	struct seat *seat;
-	struct wlr_tablet_pad *tablet;
+	struct wlr_tablet_pad *pad;
+	struct wlr_tablet_v2_tablet_pad *pad_v2;
+	struct drawing_tablet *tablet;
+	struct wlr_surface *current_surface;
 	struct {
+		struct wl_listener current_surface_destroy;
 		struct wl_listener button;
+		struct wl_listener ring;
+		struct wl_listener strip;
 		struct wl_listener destroy;
 	} handlers;
 	struct wl_list link; /* seat.tablet_pads */
 };
 
 void tablet_pad_init(struct seat *seat, struct wlr_input_device *wlr_input_device);
+void tablet_pad_attach_tablet(struct seat *seat);
+void tablet_pad_enter_surface(struct seat *seat, struct wlr_surface *wlr_surface);
 
 #endif /* LABWC_TABLET_PAD_H */

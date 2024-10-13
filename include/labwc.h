c4aa3fe3e65fe2e28e7c5a882c0e8e79b72e51d0
Jens Peters <jp7677@gmail.com>
2024-10-13 14:13:01 +0200

    server: add dmabuf feedback
    Co-authored-by: Consolatis <35009135+Consolatis@users.noreply.github.com>

--- include/labwc.h
+++ include/labwc.h
@@ -225,6 +225,7 @@ struct server {
 		struct wlr_backend *backend;
 	} headless;
 	struct wlr_session *session;
+	struct wlr_linux_dmabuf_v1 *linux_dmabuf;
 
 	struct wlr_xdg_shell *xdg_shell;
 	struct wlr_layer_shell_v1 *layer_shell;

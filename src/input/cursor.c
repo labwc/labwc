d6eeefe2346a6fd0f1868b4cab609792155eece8
tokyo4j <hrak1529@gmail.com>
2024-10-17 23:27:56 +0900

    cursor: send a frame event after emulated button events
    This fixes the issue that emulated tablet/touchscreen button events
    sometimes don't take effect on applications immediately.

--- src/input/cursor.c
+++ src/input/cursor.c
@@ -1225,6 +1225,7 @@ cursor_emulate_button(struct seat *seat, uint32_t button,
 		cursor_finish_button_release(seat, button);
 		break;
 	}
+	wlr_seat_pointer_notify_frame(seat->seat);
 }
 
 static int

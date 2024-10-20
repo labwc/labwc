7912665b0f3a541e5d766c7942446716df5e839d
John Lindgren <john@jlindgren.net>
2024-10-20 02:55:12 -0400

    output: remove ported wlr_output functions
    We have several wlr_output_* functions which are just wrappers around
    corresponding wlr_output_state_* functions and don't actually touch the
    wlr_output itself. These probably made some sense historically, but IMHO
    they are just confusing now. So remove them and call wlr_output_state_*
    directly.
    
    Rename wlr_output_commit() (still useful) to output_state_commit().

--- include/output-state.h
+++ include/output-state.h
@@ -5,30 +5,9 @@
 #include <stdbool.h>
 
 struct output;
-struct wlr_output;
 
 void output_state_init(struct output *output);
 
-/* Forward port of removed functions */
-
-bool wlr_output_test(struct wlr_output *wlr_output);
-
-bool wlr_output_commit(struct wlr_output *wlr_output);
-
-void wlr_output_enable(struct wlr_output *wlr_output, bool enabled);
-
-void wlr_output_set_mode(struct wlr_output *wlr_output,
-	struct wlr_output_mode *mode);
-
-void wlr_output_set_custom_mode(struct wlr_output *wlr_output,
-		int32_t width, int32_t height, int32_t refresh);
-
-void wlr_output_set_scale(struct wlr_output *wlr_output, float scale);
-
-void wlr_output_set_transform(struct wlr_output *wlr_output,
-	enum wl_output_transform transform);
-
-void wlr_output_enable_adaptive_sync(struct wlr_output *wlr_output,
-	bool enabled);
+bool output_state_commit(struct output *output);
 
 #endif // LABWC_OUTPUT_STATE_H
--- src/output-state.c
+++ src/output-state.c
@@ -27,17 +27,10 @@ output_state_init(struct output *output)
 }
 
 bool
-wlr_output_test(struct wlr_output *wlr_output)
+output_state_commit(struct output *output)
 {
-	struct output *output = wlr_output->data;
-	return wlr_output_test_state(wlr_output, &output->pending);
-}
-
-bool
-wlr_output_commit(struct wlr_output *wlr_output)
-{
-	struct output *output = wlr_output->data;
-	bool committed = wlr_output_commit_state(wlr_output, &output->pending);
+	bool committed =
+		wlr_output_commit_state(output->wlr_output, &output->pending);
 	if (committed) {
 		wlr_output_state_finish(&output->pending);
 		wlr_output_state_init(&output->pending);
@@ -45,48 +38,4 @@ wlr_output_commit(struct wlr_output *wlr_output)
 		wlr_log(WLR_ERROR, "Failed to commit frame");
 	}
 	return committed;
-}
-
-void
-wlr_output_enable(struct wlr_output *wlr_output, bool enabled)
-{
-	struct output *output = wlr_output->data;
-	wlr_output_state_set_enabled(&output->pending, enabled);
-}
-
-void
-wlr_output_set_mode(struct wlr_output *wlr_output, struct wlr_output_mode *mode)
-{
-	struct output *output = wlr_output->data;
-	wlr_output_state_set_mode(&output->pending, mode);
-}
-
-void
-wlr_output_set_custom_mode(struct wlr_output *wlr_output,
-		int32_t width, int32_t height, int32_t refresh)
-{
-	struct output *output = wlr_output->data;
-	wlr_output_state_set_custom_mode(&output->pending, width, height, refresh);
-}
-
-void
-wlr_output_set_scale(struct wlr_output *wlr_output, float scale)
-{
-	struct output *output = wlr_output->data;
-	wlr_output_state_set_scale(&output->pending, scale);
-}
-
-void
-wlr_output_set_transform(struct wlr_output *wlr_output,
-		enum wl_output_transform transform)
-{
-	struct output *output = wlr_output->data;
-	wlr_output_state_set_transform(&output->pending, transform);
-}
-
-void
-wlr_output_enable_adaptive_sync(struct wlr_output *wlr_output, bool enabled)
-{
-	struct output *output = wlr_output->data;
-	wlr_output_state_set_adaptive_sync_enabled(&output->pending, enabled);
 }

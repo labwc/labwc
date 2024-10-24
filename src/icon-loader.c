20d909303ee0da22945a574659f83340c6aa9ec2
Simon Long <simon@raspberrypi.com>
2024-10-24 18:31:11 +0100

    Use caseless compare

--- src/icon-loader.c
+++ src/icon-loader.c
@@ -245,7 +245,7 @@ get_db_entry_by_id_fuzzy(struct sfdo_desktop_db *db, const char *app_id)
 		int alen = strlen(app_id);
 		int dlen = strlen(desktop_id_base);
 
-		if (!strncmp(app_id, desktop_id, alen > dlen ? dlen : alen)) {
+		if (!strncasecmp(app_id, desktop_id, alen > dlen ? dlen : alen)) {
 			return entry;
 		}
 	}

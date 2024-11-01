d4bb77060cc4cf31d88b2d01f69dde5ad3b10bcc
Jens Peters <jp7677@gmail.com>
2024-11-01 09:11:33 +0100

    input: close xdg-popups on touch down
    Taken over and now shared from the tablet implementation.

--- include/labwc.h
+++ include/labwc.h
@@ -489,6 +489,8 @@ void seat_finish(struct server *server);
 void seat_reconfigure(struct server *server);
 void seat_focus_surface(struct seat *seat, struct wlr_surface *surface);
 
+void seat_pointer_end_grab(struct seat *seat, struct wlr_surface *surface);
+
 /**
  * seat_focus_lock_surface() - ONLY to be called from session-lock.c to
  * focus lock screen surfaces. Use seat_focus_surface() otherwise.

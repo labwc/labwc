6ecbef9c84669fbc1062c5dc3102e86f33bf26f2
tokyo4j <hrak1529@gmail.com>
2024-11-12 09:17:04 +0900

    theme: reorder theme entries in documentation and codebase

--- include/theme.h
+++ include/theme.h
@@ -45,8 +45,6 @@ struct theme {
 	int window_titlebar_padding_height;
 
 	int title_height;
-	int menu_overlap_x;
-	int menu_overlap_y;
 
 	/* colors */
 	float window_active_border_color[4];
@@ -60,7 +58,6 @@ struct theme {
 	float window_active_label_text_color[4];
 	float window_inactive_label_text_color[4];
 	enum lab_justification window_label_text_justify;
-	enum lab_justification menu_title_text_justify;
 
 	/* buttons */
 	int window_button_width;
@@ -69,50 +66,6 @@ struct theme {
 
 	/* the corner radius of the hover effect */
 	int window_button_hover_bg_corner_radius;
-
-	int menu_item_padding_x;
-	int menu_item_padding_y;
-	int menu_item_height;
-
-	int menu_header_height;
-
-	float menu_items_bg_color[4];
-	float menu_items_text_color[4];
-	float menu_items_active_bg_color[4];
-	float menu_items_active_text_color[4];
-
-	int menu_min_width;
-	int menu_max_width;
-
-	int menu_separator_line_thickness;
-	int menu_separator_padding_width;
-	int menu_separator_padding_height;
-	float menu_separator_color[4];
-
-	float menu_title_bg_color[4];
-
-	float menu_title_text_color[4];
-
-	int osd_border_width;
-
-	float osd_bg_color[4];
-	float osd_border_color[4];
-	float osd_label_text_color[4];
-
-	int osd_window_switcher_width;
-	int osd_window_switcher_padding;
-	int osd_window_switcher_item_padding_x;
-	int osd_window_switcher_item_padding_y;
-	int osd_window_switcher_item_active_border_width;
-	bool osd_window_switcher_width_is_percent;
-	int osd_window_switcher_preview_border_width;
-	float osd_window_switcher_preview_border_color[3][4];
-
-	int osd_workspace_switcher_boxes_width;
-	int osd_workspace_switcher_boxes_height;
-
-	struct theme_snapping_overlay
-		snapping_overlay_region, snapping_overlay_edge;
 
 	/* window drop-shadows */
 	int window_active_shadow_size;
@@ -139,8 +92,52 @@ struct theme {
 
 	} window[2]; /* indexed by THEME_INACTIVE and THEME_ACTIVE */
 
-	/* textures */
+	int menu_item_height;
+	int menu_header_height;
 
+	int menu_overlap_x;
+	int menu_overlap_y;
+	int menu_min_width;
+	int menu_max_width;
+
+	int menu_item_padding_x;
+	int menu_item_padding_y;
+	float menu_items_bg_color[4];
+	float menu_items_text_color[4];
+	float menu_items_active_bg_color[4];
+	float menu_items_active_text_color[4];
+
+	int menu_separator_line_thickness;
+	int menu_separator_padding_width;
+	int menu_separator_padding_height;
+	float menu_separator_color[4];
+
+	float menu_title_bg_color[4];
+	enum lab_justification menu_title_text_justify;
+	float menu_title_text_color[4];
+
+	int osd_border_width;
+
+	float osd_bg_color[4];
+	float osd_border_color[4];
+	float osd_label_text_color[4];
+
+	int osd_window_switcher_width;
+	int osd_window_switcher_padding;
+	int osd_window_switcher_item_padding_x;
+	int osd_window_switcher_item_padding_y;
+	int osd_window_switcher_item_active_border_width;
+	bool osd_window_switcher_width_is_percent;
+	int osd_window_switcher_preview_border_width;
+	float osd_window_switcher_preview_border_color[3][4];
+
+	int osd_workspace_switcher_boxes_width;
+	int osd_workspace_switcher_boxes_height;
+
+	struct theme_snapping_overlay
+		snapping_overlay_region, snapping_overlay_edge;
+
+	/* textures */
 	struct lab_data_buffer *corner_top_left_active_normal;
 	struct lab_data_buffer *corner_top_right_active_normal;
 	struct lab_data_buffer *corner_top_left_inactive_normal;

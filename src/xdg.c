1dc4e7ed28ba36793b9d6decd89f203d686bc53b
Consolatis <35009135+Consolatis@users.noreply.github.com>
2024-11-27 04:14:08 +0100

    chase wlroots: wlr_xdg_surface_get_geometry remove MR 4788
    Ref: 5c98d1a04a1439bf40c6e516086cfaff2d67f135
    ("xdg-surface: fix window geometry handling")

--- src/xdg.c
+++ src/xdg.c
@@ -149,8 +149,7 @@ handle_commit(struct wl_listener *listener, void *data)
 		return;
 	}
 
-	struct wlr_box size;
-	wlr_xdg_surface_get_geometry(xdg_surface, &size);
+	struct wlr_box size = xdg_surface->geometry;
 	bool update_required = false;
 
 	/*
@@ -727,10 +726,8 @@ xdg_toplevel_view_map(struct view *view)
 		 * dimensions remain zero until handle_commit().
 		 */
 		if (wlr_box_empty(&view->pending)) {
-			struct wlr_box size;
-			wlr_xdg_surface_get_geometry(xdg_surface, &size);
-			view->pending.width = size.width;
-			view->pending.height = size.height;
+			view->pending.width = xdg_surface->geometry.width;
+			view->pending.height = xdg_surface->geometry.height;
 		}
 
 		/*

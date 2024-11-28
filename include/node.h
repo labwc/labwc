9a3412324db8046a7355676a1eaba542a47b76a9
tokyo4j <hrak1529@gmail.com>
2024-11-28 21:34:24 +0900

    node: add node_scaled_scene_buffer_from_node

--- include/node.h
+++ include/node.h
@@ -8,6 +8,7 @@ struct lab_layer_surface;
 struct lab_layer_popup;
 struct menuitem;
 struct ssd_button;
+struct scaled_scene_buffer;
 
 enum node_descriptor_type {
 	LAB_NODE_DESC_NODE = 0,
@@ -19,6 +20,7 @@ enum node_descriptor_type {
 	LAB_NODE_DESC_IME_POPUP,
 	LAB_NODE_DESC_MENUITEM,
 	LAB_NODE_DESC_TREE,
+	LAB_NODE_DESC_SCALED_SCENE_BUFFER,
 	LAB_NODE_DESC_SSD_BUTTON,
 };
 
@@ -79,6 +81,13 @@ struct menuitem *node_menuitem_from_node(
  * @wlr_scene_node: wlr_scene_node from which to return data
  */
 struct ssd_button *node_ssd_button_from_node(
+	struct wlr_scene_node *wlr_scene_node);
+
+/**
+ * node_scaled_scene_buffer_from_node - return scaled_scene_buffer from node
+ * @wlr_scene_node: wlr_scene_node from which to return data
+ */
+struct scaled_scene_buffer *node_scaled_scene_buffer_from_node(
 	struct wlr_scene_node *wlr_scene_node);
 
 #endif /* LABWC_NODE_DESCRIPTOR_H */

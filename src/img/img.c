labwc/labwc

commit 16dbdc64e58d66011bbf319b92de844dab0ca8d9

ssd: rework titlebar button rendering
- fix that icons for normal/hovered/rounded buttons are not placed
  exactly the same position
- fix blurry window button icons in scaled outputs

This commit introduces lab_img and scaled_img_buffer and uses them for
rendering icons in the window titlebar. Now the process of rendering
button icons are split into 2 phases: loading with lab_img_load() and
creating scene-nodes for them with scaled_img_buffer_create(). This
might incur some additional overhead since we no longer preload icon
textures, but the rendering of icon only happens for the first window
as backing buffers are shared and the overhead won't be noticeable.
This commit also simplifies the process of centering icon buffer in the
button, by creating icon buffers in a fixed geometry via
lab_img_render().

src/img/img.c
@@ -1,0 +1,222 @@
+// SPDX-License-Identifier: GPL-2.0-only
+
+#include <assert.h>
+#include <wlr/util/log.h>
+#include "buffer.h"
+#include "config.h"
+#include "common/box.h"
+#include "common/graphic-helpers.h"
+#include "common/macros.h"
+#include "common/mem.h"
+#include "common/string-helpers.h"
+#include "img/img.h"
+#include "img/img-png.h"
+#if HAVE_RSVG
+#include "img/img-svg.h"
+#endif
+#include "img/img-xbm.h"
+#include "img/img-xpm.h"
+#include "labwc.h"
+#include "theme.h"
+
+struct lab_img_cache {
+	enum lab_img_type type;
+	/* lab_img_cache is refcounted to be shared by multiple lab_imgs */
+	int refcount;
+
+	/* Handler for the loaded image file */
+	struct lab_data_buffer *buffer; /* for PNG/XBM/XPM image */
+#if HAVE_RSVG
+	RsvgHandle *svg; /* for SVG image */
+#endif
+};
+
+static struct lab_img *
+create_img(struct lab_img_cache *cache)
+{
+	struct lab_img *img = znew(*img);
+	img->cache = cache;
+	cache->refcount++;
+	wl_array_init(&img->modifiers);
+	return img;
+}
+
+struct lab_img *
+lab_img_load(enum lab_img_type type, const char *path, float *xbm_color)
+{
+	if (string_null_or_empty(path)) {
+		return NULL;
+	}
+
+	struct lab_img_cache *cache = znew(*cache);
+	cache->type = type;
+
+	switch (type) {
+	case LAB_IMG_PNG:
+		cache->buffer = img_png_load(path);
+		break;
+	case LAB_IMG_XBM:
+		assert(xbm_color);
+		cache->buffer = img_xbm_load(path, xbm_color);
+		break;
+	case LAB_IMG_XPM:
+		cache->buffer = img_xpm_load(path);
+		break;
+	case LAB_IMG_SVG:
+#if HAVE_RSVG
+		cache->svg = img_svg_load(path);
+#endif
+		break;
+	}
+
+	bool img_is_loaded = (bool)cache->buffer;
+#if HAVE_RSVG
+	img_is_loaded |= (bool)cache->svg;
+#endif
+
+	if (img_is_loaded) {
+		return create_img(cache);
+	} else {
+		free(cache);
+		return NULL;
+	}
+}
+
+struct lab_img *
+lab_img_load_from_bitmap(const char *bitmap, float *rgba)
+{
+	struct lab_data_buffer *buffer = img_xbm_load_from_bitmap(bitmap, rgba);
+	if (!buffer) {
+		return NULL;
+	}
+
+	struct lab_img_cache *cache = znew(*cache);
+	cache->type = LAB_IMG_XBM;
+	cache->buffer = buffer;
+
+	return create_img(cache);
+}
+
+struct lab_img *
+lab_img_copy(struct lab_img *img)
+{
+	struct lab_img *new_img = create_img(img->cache);
+	wl_array_copy(&new_img->modifiers, &img->modifiers);
+	return new_img;
+}
+
+void
+lab_img_add_modifier(struct lab_img *img,  lab_img_modifier_func_t modifier,
+	struct theme *theme)
+{
+	img->theme = theme;
+	lab_img_modifier_func_t *mod = wl_array_add(&img->modifiers, sizeof(*mod));
+	*mod = modifier;
+}
+
+/*
+ * Takes a source surface from PNG/XBM/XPM file and output a buffer for the
+ * given size. The source surface is placed at the center of the output buffer
+ * and shrunk if it overflows from the output buffer.
+ */
+static struct lab_data_buffer *
+render_cairo_surface(cairo_surface_t *surface, int width, int height,
+	int padding, double scale)
+{
+	assert(surface);
+	int src_w = cairo_image_surface_get_width(surface);
+	int src_h = cairo_image_surface_get_height(surface);
+
+	struct lab_data_buffer *buffer =
+		buffer_create_cairo(width, height, scale);
+	cairo_t *cairo = cairo_create(buffer->surface);
+
+	struct wlr_box container = {
+		.x = padding,
+		.y = padding,
+		.width = width - 2 * padding,
+		.height = height - 2 * padding,
+	};
+
+	struct wlr_box src_box = box_fit_within(src_w, src_h, &container);
+	double scene_scale = MIN(1.0, (double_t)container.width / (double)src_w);
+	cairo_translate(cairo, src_box.x, src_box.y);
+	cairo_scale(cairo, scene_scale, scene_scale);
+	cairo_set_source_surface(cairo, surface, 0, 0);
+	cairo_pattern_set_filter(cairo_get_source(cairo), CAIRO_FILTER_GOOD);
+	cairo_set_operator(cairo, CAIRO_OPERATOR_SOURCE);
+	cairo_paint(cairo);
+
+	cairo_destroy(cairo);
+
+	return buffer;
+}
+
+struct lab_data_buffer *
+lab_img_render(struct lab_img *img, int width, int height, int padding,
+	double scale)
+{
+	struct lab_data_buffer *buffer = NULL;
+
+	/* Render the image into the buffer for the given size */
+	switch (img->cache->type) {
+	case LAB_IMG_PNG:
+	case LAB_IMG_XBM:
+	case LAB_IMG_XPM:
+		buffer = render_cairo_surface(img->cache->buffer->surface,
+			width, height, padding, scale);
+		break;
+#if HAVE_RSVG
+	case LAB_IMG_SVG:
+		buffer = img_svg_render(img->cache->svg, width, height,
+			padding, scale);
+		break;
+#endif
+	default:
+		break;
+	}
+
+	if (!buffer) {
+		return NULL;
+	}
+
+	/* Apply modifiers to the buffer (e.g. draw hover overlay) */
+	cairo_t *cairo = cairo_create(buffer->surface);
+	lab_img_modifier_func_t *modifier;
+	wl_array_for_each(modifier, &img->modifiers) {
+		cairo_save(cairo);
+		(*modifier)(img->theme, cairo, width, height);
+		cairo_restore(cairo);
+	}
+
+	cairo_surface_flush(buffer->surface);
+	cairo_destroy(cairo);
+
+	return buffer;
+}
+
+void
+lab_img_destroy(struct lab_img *img)
+{
+	if (!img) {
+		return;
+	}
+
+	struct lab_img_cache *cache = img->cache;
+	cache->refcount--;
+
+	if (cache->refcount == 0) {
+		if (cache->buffer) {
+			wlr_buffer_drop(&cache->buffer->base);
+		}
+#if HAVE_RSVG
+		if (cache->svg) {
+			g_object_unref(cache->svg);
+		}
+#endif
+		free(cache);
+	}
+
+	wl_array_release(&img->modifiers);
+	free(img);
+}

include/buffer.h
@@ -62,18 +62,6 @@ struct lab_data_buffer *buffer_create_cairo(uint32_t logical_width,
 	uint32_t logical_height, float scale);
 
 /*
- * Create a buffer from an image surface, for display as an icon.
- *
- * The surface is either adopted by the buffer (which takes ownership),
- * or copied and then destroyed.
- *
- * This function allows non-ARGB32 source images and converts to
- * CAIRO_FORMAT_ARGB32 if needed.
- */
-struct lab_data_buffer *buffer_convert_cairo_surface_for_icon(
-	cairo_surface_t *surface, uint32_t icon_size, float scale);
-
-/*
  * Create a buffer which holds (and takes ownership of) raw pixel data
  * in pre-multiplied ARGB32 format.
  *

include/common/box.h
@@ -13,14 +13,13 @@ void box_union(struct wlr_box *box_dest, struct wlr_box *box_a,
 	struct wlr_box *box_b);
 
 /*
- * Fits and centers a content box (width & height) within a bounding box
- * (max_width & max_height). The content box is downscaled if necessary
- * (preserving aspect ratio) but not upscaled.
+ * Fits and centers a content box (width & height) within a bounding box.
+ * The content box is downscaled if necessary (preserving aspect ratio) but
+ * not upscaled.
  *
  * The returned x & y coordinates are the centered content position
  * relative to the top-left corner of the bounding box.
  */
-struct wlr_box box_fit_within(int width, int height, int max_width,
-	int max_height);
+struct wlr_box box_fit_within(int width, int height, struct wlr_box *bounding_box);
 
 #endif /* LABWC_BOX_H */

include/common/scaled-img-buffer.h
@@ -1,0 +1,37 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+#ifndef LABWC_SCALED_IMG_BUFFER_H
+#define LABWC_SCALED_IMG_BUFFER_H
+
+#include <stdbool.h>
+
+struct wlr_scene_tree;
+struct wlr_scene_node;
+struct wlr_scene_buffer;
+struct lab_img;
+
+struct scaled_img_buffer {
+	struct scaled_scene_buffer *scaled_buffer;
+	struct wlr_scene_buffer *scene_buffer;
+	struct lab_img *img;
+	int width;
+	int height;
+	int padding;
+};
+
+/*
+ * Create an auto scaling image buffer, providing a wlr_scene_buffer node for
+ * display. It gets destroyed automatically when the backing scaled_scene_buffer
+ * is being destroyed which in turn happens automatically when the backing
+ * wlr_scene_buffer (or one of its parents) is being destroyed.
+ */
+struct scaled_img_buffer *scaled_img_buffer_create(struct wlr_scene_tree *parent,
+	struct lab_img *img, int width, int height, int padding);
+
+/* Update image, width, height and padding of the scaled_img_buffer */
+void scaled_img_buffer_update(struct scaled_img_buffer *self,
+	struct lab_img *img, int width, int height, int padding);
+
+/* Obtain scaled_img_buffer from wlr_scene_node */
+struct scaled_img_buffer *scaled_img_buffer_from_node(struct wlr_scene_node *node);
+
+#endif /* LABWC_SCALED_IMG_BUFFER_H */

include/desktop-entry.h
@@ -7,7 +7,7 @@ struct server;
 void desktop_entry_init(struct server *server);
 void desktop_entry_finish(struct server *server);
 
-struct lab_data_buffer *desktop_entry_icon_lookup(struct server *server,
+struct lab_img *desktop_entry_icon_lookup(struct server *server,
 	const char *app_id, int size, float scale);
 
 /**

include/img/img-png.h
@@ -2,9 +2,6 @@
 #ifndef LABWC_IMG_PNG_H
 #define LABWC_IMG_PNG_H
 
-struct lab_data_buffer;
-
-void img_png_load(const char *filename, struct lab_data_buffer **buffer,
-	int size, float scale);
+struct lab_data_buffer *img_png_load(const char *filename);
 
 #endif /* LABWC_IMG_PNG_H */

include/img/img-svg.h
@@ -2,9 +2,13 @@
 #ifndef LABWC_IMG_SVG_H
 #define LABWC_IMG_SVG_H
 
+#include <librsvg/rsvg.h>
+
 struct lab_data_buffer;
 
-void img_svg_load(const char *filename, struct lab_data_buffer **buffer,
-	int size, float scale);
+RsvgHandle *img_svg_load(const char *filename);
+
+struct lab_data_buffer *img_svg_render(RsvgHandle *svg, int w, int h,
+	int padding, double scale);
 
 #endif /* LABWC_IMG_SVG_H */

include/img/img-xbm.h
@@ -5,18 +5,15 @@
 struct lab_data_buffer;
 
 /**
- * img_xbm_from_bitmap() - create button from monochrome bitmap
+ * img_xbm_load_from_bitmap() - create button from monochrome bitmap
  * @bitmap: bitmap data array in hexadecimal xbm format
- * @buffer: cairo-surface-buffer to create
  * @rgba: color
  *
  * Example bitmap: char button[6] = { 0x3f, 0x3f, 0x21, 0x21, 0x21, 0x3f };
  */
-void img_xbm_from_bitmap(const char *bitmap, struct lab_data_buffer **buffer,
-	float *rgba);
+struct lab_data_buffer *img_xbm_load_from_bitmap(const char *bitmap, float *rgba);
 
 /* img_xbm_load - Convert xbm file to buffer with cairo surface */
-void img_xbm_load(const char *filename, struct lab_data_buffer **buffer,
-	float *rgba);
+struct lab_data_buffer *img_xbm_load(const char *filename, float *rgba);
 
 #endif /* LABWC_IMG_XBM_H */

commit 4eaaa0e1219fc759640ea7beaa97acd9f7f88ea9

img: fix scaling for rendering large image in non-square rectangle
16dbdc6 caused a regression that the vscode icon (1024x1024) in the
titlebar is cut off with following configuraion:

  window.button.width: 26
  window.button.height: 16

src/img/img.c
@@ -138,9 +138,9 @@ render_cairo_surface(cairo_surface_t *surface, int width, int height,
 		.height = height - 2 * padding,
 	};
 
-	struct wlr_box src_box = box_fit_within(src_w, src_h, &container);
-	double scene_scale = MIN(1.0, (double_t)container.width / (double)src_w);
-	cairo_translate(cairo, src_box.x, src_box.y);
+	struct wlr_box dst_box = box_fit_within(src_w, src_h, &container);
+	double scene_scale = (double)dst_box.width / (double)src_w;
+	cairo_translate(cairo, dst_box.x, dst_box.y);
 	cairo_scale(cairo, scene_scale, scene_scale);
 	cairo_set_source_surface(cairo, surface, 0, 0);
 	cairo_pattern_set_filter(cairo_get_source(cairo), CAIRO_FILTER_GOOD);
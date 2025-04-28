d420e9b6eaa1b9bba4482739b81c27c972752f99
Johan Malm <jgm323@gmail.com>
2025-04-28 20:38:19 +0100

    Add labnag
    Based on swaynag (https://github.com/swaywm/sway/tree/master/swaynag)
    
    Copied at commit:
    https://github.com/swaywm/sway/commit/03483ff3707a358d935e451d39748e58c205ce8a
    
    Contains the following modifiations:
    
    - Some functional changes including:
      - Disable exclusive-zone by default (Written-by: @Consolatis) and add
        command line option -x|--exclusive-zone
      - Add close timeout (Written-by: @Consolatis) and -t|--timeout option
      - Use index of button (from right-to-left) for exit code
      - Disable reading from config file and remove associated --type option
    - Refactoring including:
      - Use wlr_log() instead of the log.{c,h} functions
      - Use wl_list instead of sway's list.c implementation
      - In the pango wrapper functions, use glib's g_strdup_vprintf() rather
        than the original stringop.c functions
    - Align with labwc coding style to pass checkpatch.pl
    - Re-licenced from MIT to GPL-2.0, and add Copyright notices for original
      authors
    
    v2
    
    - Remove option -s|--dismiss-button and the default "X" button. To get
      such a button, "-Z X :"
    - Remove options -b and -z because there is no requirement to run
      in a terminal.
    - Remove *-no-terminal from options --button and --button-dismiss because
      commands are now always run directly without a terminal.

---
+++ clients/pool-buffer.c
@@ -0,0 +1,145 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copied from https://github.com/swaywm/sway
+ *
+ * Copyright (C) 2016-2017 Drew DeVault
+ */
+#define _POSIX_C_SOURCE 200809L
+#include <assert.h>
+#include <cairo.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <pango/pangocairo.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <time.h>
+#include <unistd.h>
+#include <wayland-client.h>
+#include "pool-buffer.h"
+
+static int anonymous_shm_open(void)
+{
+	int retries = 100;
+
+	do {
+		// try a probably-unique name
+		struct timespec ts;
+		clock_gettime(CLOCK_MONOTONIC, &ts);
+		pid_t pid = getpid();
+		char name[50];
+		snprintf(name, sizeof(name), "/labnag-%x-%x",
+			(unsigned int)pid, (unsigned int)ts.tv_nsec);
+
+		// shm_open guarantees that O_CLOEXEC is set
+		int fd = shm_open(name, O_RDWR | O_CREAT | O_EXCL, 0600);
+		if (fd >= 0) {
+			shm_unlink(name);
+			return fd;
+		}
+
+		--retries;
+	} while (retries > 0 && errno == EEXIST);
+
+	return -1;
+}
+
+static void buffer_release(void *data, struct wl_buffer *wl_buffer)
+{
+	struct pool_buffer *buffer = data;
+	buffer->busy = false;
+}
+
+static const struct wl_buffer_listener buffer_listener = {
+	.release = buffer_release
+};
+
+static struct pool_buffer *create_buffer(struct wl_shm *shm,
+		struct pool_buffer *buf, int32_t width, int32_t height,
+		uint32_t format)
+{
+	uint32_t stride = width * 4;
+	size_t size = stride * height;
+
+	int fd = anonymous_shm_open();
+	if (fd == -1) {
+		return NULL;
+	}
+	if (ftruncate(fd, size) < 0) {
+		close(fd);
+		return NULL;
+	}
+	void *data = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	struct wl_shm_pool *pool = wl_shm_create_pool(shm, fd, size);
+	buf->buffer = wl_shm_pool_create_buffer(pool, 0,
+			width, height, stride, format);
+	wl_shm_pool_destroy(pool);
+	close(fd);
+
+	buf->size = size;
+	buf->width = width;
+	buf->height = height;
+	buf->data = data;
+	buf->surface = cairo_image_surface_create_for_data(data,
+			CAIRO_FORMAT_ARGB32, width, height, stride);
+	buf->cairo = cairo_create(buf->surface);
+	buf->pango = pango_cairo_create_context(buf->cairo);
+
+	wl_buffer_add_listener(buf->buffer, &buffer_listener, buf);
+	return buf;
+}
+
+void destroy_buffer(struct pool_buffer *buffer)
+{
+	if (buffer->buffer) {
+		wl_buffer_destroy(buffer->buffer);
+		buffer->buffer = NULL;
+	}
+	if (buffer->cairo) {
+		cairo_destroy(buffer->cairo);
+		buffer->cairo = NULL;
+	}
+	if (buffer->surface) {
+		cairo_surface_destroy(buffer->surface);
+		buffer->surface = NULL;
+	}
+	if (buffer->pango) {
+		g_object_unref(buffer->pango);
+		buffer->pango = NULL;
+	}
+	if (buffer->data) {
+		munmap(buffer->data, buffer->size);
+		buffer->data = NULL;
+	}
+}
+
+struct pool_buffer *get_next_buffer(struct wl_shm *shm,
+		struct pool_buffer pool[static 2], uint32_t width, uint32_t height)
+{
+	struct pool_buffer *buffer = NULL;
+
+	for (size_t i = 0; i < 2; ++i) {
+		if (pool[i].busy) {
+			continue;
+		}
+		buffer = &pool[i];
+	}
+
+	if (!buffer) {
+		return NULL;
+	}
+
+	if (buffer->width != width || buffer->height != height) {
+		destroy_buffer(buffer);
+	}
+
+	if (!buffer->buffer) {
+		if (!create_buffer(shm, buffer, width, height,
+					WL_SHM_FORMAT_ARGB8888)) {
+			return NULL;
+		}
+	}
+	buffer->busy = true;
+	return buffer;
+}

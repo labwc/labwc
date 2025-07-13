labwc/labwc

commit 16dbdc64e58d66011bbf319b92de844dab0ca8d9

ssd: rework titlebar button rendering
- fix that icons for normal/hovered/rounded buttons are not placed
  exactly the same position
- fix blurry window button icons in scaled outputs

This commit introduces lab_img and scaled_img_buffer and uses them for
rendering icons in the window titlebar. Now the process of rendering
button icons are split into 2 phases: loading with lab_img_load() and
creating scene-nodes for them with scaled_img_buffer_create(). This
might incur some additional overhead since we no longer preload icon
textures, but the rendering of icon only happens for the first window
as backing buffers are shared and the overhead won't be noticeable.
This commit also simplifies the process of centering icon buffer in the
button, by creating icon buffers in a fixed geometry via
lab_img_render().

src/img/img-svg.c
@@ -10,24 +10,18 @@
 #include <stdlib.h>
 #include <wlr/util/log.h>
 #include "buffer.h"
-#include "img/img-svg.h"
 #include "common/string-helpers.h"
+#include "img/img-svg.h"
 #include "labwc.h"
 
-void
-img_svg_load(const char *filename, struct lab_data_buffer **buffer, int size,
-		float scale)
+RsvgHandle *
+img_svg_load(const char *filename)
 {
-	if (*buffer) {
-		wlr_buffer_drop(&(*buffer)->base);
-		*buffer = NULL;
-	}
 	if (string_null_or_empty(filename)) {
-		return;
+		return NULL;
 	}
 
 	GError *err = NULL;
-	RsvgRectangle viewport = { .width = size, .height = size };
 	RsvgHandle *svg = rsvg_handle_new_from_file(filename, &err);
 	if (err) {
 		wlr_log(WLR_DEBUG, "error reading svg %s-%s", filename, err->message);
@@ -36,33 +30,43 @@ img_svg_load(const char *filename, struct lab_data_buffer **buffer, int size,
 		 * rsvg_handle_new_from_file() returns NULL if an error occurs,
 		 * so there is no need to free svg here.
 		 */
-		return;
+		return NULL;
 	}
+	return svg;
+}
 
-	*buffer = buffer_create_cairo(size, size, scale);
-	cairo_surface_t *image = (*buffer)->surface;
+struct lab_data_buffer *
+img_svg_render(RsvgHandle *svg, int w, int h, int padding, double scale)
+{
+	struct lab_data_buffer *buffer = buffer_create_cairo(w, h, scale);
+	cairo_surface_t *image = buffer->surface;
 	cairo_t *cr = cairo_create(image);
+	GError *err = NULL;
 
+	RsvgRectangle viewport = {
+		.x = padding,
+		.y = padding,
+		.width = w - 2 * padding,
+		.height = h - 2 * padding,
+	};
 	rsvg_handle_render_document(svg, cr, &viewport, &err);
 	if (err) {
-		wlr_log(WLR_ERROR, "error rendering svg %s-%s", filename, err->message);
+		wlr_log(WLR_ERROR, "error rendering svg: %s", err->message);
 		g_error_free(err);
 		goto error;
 	}
-
 	if (cairo_surface_status(image)) {
-		wlr_log(WLR_ERROR, "error reading svg button '%s'", filename);
+		wlr_log(WLR_ERROR, "error reading svg file");
 		goto error;
 	}
-	cairo_surface_flush(image);
+	cairo_surface_flush(buffer->surface);
 	cairo_destroy(cr);
 
-	g_object_unref(svg);
-	return;
+	return buffer;
 
 error:
-	wlr_buffer_drop(&(*buffer)->base);
-	*buffer = NULL;
+	wlr_buffer_drop(&buffer->base);
 	cairo_destroy(cr);
 	g_object_unref(svg);
+	return NULL;
 }

include/buffer.h
@@ -62,18 +62,6 @@ struct lab_data_buffer *buffer_create_cairo(uint32_t logical_width,
 	uint32_t logical_height, float scale);
 
 /*
- * Create a buffer from an image surface, for display as an icon.
- *
- * The surface is either adopted by the buffer (which takes ownership),
- * or copied and then destroyed.
- *
- * This function allows non-ARGB32 source images and converts to
- * CAIRO_FORMAT_ARGB32 if needed.
- */
-struct lab_data_buffer *buffer_convert_cairo_surface_for_icon(
-	cairo_surface_t *surface, uint32_t icon_size, float scale);
-
-/*
  * Create a buffer which holds (and takes ownership of) raw pixel data
  * in pre-multiplied ARGB32 format.
  *

include/common/box.h
@@ -13,14 +13,13 @@ void box_union(struct wlr_box *box_dest, struct wlr_box *box_a,
 	struct wlr_box *box_b);
 
 /*
- * Fits and centers a content box (width & height) within a bounding box
- * (max_width & max_height). The content box is downscaled if necessary
- * (preserving aspect ratio) but not upscaled.
+ * Fits and centers a content box (width & height) within a bounding box.
+ * The content box is downscaled if necessary (preserving aspect ratio) but
+ * not upscaled.
  *
  * The returned x & y coordinates are the centered content position
  * relative to the top-left corner of the bounding box.
  */
-struct wlr_box box_fit_within(int width, int height, int max_width,
-	int max_height);
+struct wlr_box box_fit_within(int width, int height, struct wlr_box *bounding_box);
 
 #endif /* LABWC_BOX_H */

include/common/scaled-img-buffer.h
@@ -1,0 +1,37 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+#ifndef LABWC_SCALED_IMG_BUFFER_H
+#define LABWC_SCALED_IMG_BUFFER_H
+
+#include <stdbool.h>
+
+struct wlr_scene_tree;
+struct wlr_scene_node;
+struct wlr_scene_buffer;
+struct lab_img;
+
+struct scaled_img_buffer {
+	struct scaled_scene_buffer *scaled_buffer;
+	struct wlr_scene_buffer *scene_buffer;
+	struct lab_img *img;
+	int width;
+	int height;
+	int padding;
+};
+
+/*
+ * Create an auto scaling image buffer, providing a wlr_scene_buffer node for
+ * display. It gets destroyed automatically when the backing scaled_scene_buffer
+ * is being destroyed which in turn happens automatically when the backing
+ * wlr_scene_buffer (or one of its parents) is being destroyed.
+ */
+struct scaled_img_buffer *scaled_img_buffer_create(struct wlr_scene_tree *parent,
+	struct lab_img *img, int width, int height, int padding);
+
+/* Update image, width, height and padding of the scaled_img_buffer */
+void scaled_img_buffer_update(struct scaled_img_buffer *self,
+	struct lab_img *img, int width, int height, int padding);
+
+/* Obtain scaled_img_buffer from wlr_scene_node */
+struct scaled_img_buffer *scaled_img_buffer_from_node(struct wlr_scene_node *node);
+
+#endif /* LABWC_SCALED_IMG_BUFFER_H */

include/desktop-entry.h
@@ -7,7 +7,7 @@ struct server;
 void desktop_entry_init(struct server *server);
 void desktop_entry_finish(struct server *server);
 
-struct lab_data_buffer *desktop_entry_icon_lookup(struct server *server,
+struct lab_img *desktop_entry_icon_lookup(struct server *server,
 	const char *app_id, int size, float scale);
 
 /**

include/img/img-png.h
@@ -2,9 +2,6 @@
 #ifndef LABWC_IMG_PNG_H
 #define LABWC_IMG_PNG_H
 
-struct lab_data_buffer;
-
-void img_png_load(const char *filename, struct lab_data_buffer **buffer,
-	int size, float scale);
+struct lab_data_buffer *img_png_load(const char *filename);
 
 #endif /* LABWC_IMG_PNG_H */

include/img/img-svg.h
@@ -2,9 +2,13 @@
 #ifndef LABWC_IMG_SVG_H
 #define LABWC_IMG_SVG_H
 
+#include <librsvg/rsvg.h>
+
 struct lab_data_buffer;
 
-void img_svg_load(const char *filename, struct lab_data_buffer **buffer,
-	int size, float scale);
+RsvgHandle *img_svg_load(const char *filename);
+
+struct lab_data_buffer *img_svg_render(RsvgHandle *svg, int w, int h,
+	int padding, double scale);
 
 #endif /* LABWC_IMG_SVG_H */

include/img/img-xbm.h
@@ -5,18 +5,15 @@
 struct lab_data_buffer;
 
 /**
- * img_xbm_from_bitmap() - create button from monochrome bitmap
+ * img_xbm_load_from_bitmap() - create button from monochrome bitmap
  * @bitmap: bitmap data array in hexadecimal xbm format
- * @buffer: cairo-surface-buffer to create
  * @rgba: color
  *
  * Example bitmap: char button[6] = { 0x3f, 0x3f, 0x21, 0x21, 0x21, 0x3f };
  */
-void img_xbm_from_bitmap(const char *bitmap, struct lab_data_buffer **buffer,
-	float *rgba);
+struct lab_data_buffer *img_xbm_load_from_bitmap(const char *bitmap, float *rgba);
 
 /* img_xbm_load - Convert xbm file to buffer with cairo surface */
-void img_xbm_load(const char *filename, struct lab_data_buffer **buffer,
-	float *rgba);
+struct lab_data_buffer *img_xbm_load(const char *filename, float *rgba);
 
 #endif /* LABWC_IMG_XBM_H */

commit e71cb66e6126ff760c828d331320da277f0c3efd

img: fix apparent double-free in img_svg_render() failure path
img_svg_render() calls g_object_unref() on the RsvgHandle in its
error path, but the handle is owned by the shared lab_img_data
struct and will be double-freed later by lab_img_destroy().

The double-free was introduced when img_svg_load() was split from
img_svg_render(). The g_object_unref() should have been removed from
img_svg_render() but was missed.

Fixes: 16dbdc64e58d66011bbf319b92de844dab0ca8d9
("ssd: rework titlebar button rendering")

src/img/img-svg.c
@@ -65,6 +65,5 @@ img_svg_render(RsvgHandle *svg, int w, int h, double scale)
 error:
 	wlr_buffer_drop(&buffer->base);
 	cairo_destroy(cr);
-	g_object_unref(svg);
 	return NULL;
 }
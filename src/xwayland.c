e6b5d91b638de9447a5e36fd11ee0d93abf5cda2
tokyo4j <hrak1529@gmail.com>
2025-09-01 17:04:41 +0900

    view: let `view_set_fallback_natural_geometry()` return wlr_box

--- src/xwayland.c
+++ src/xwayland.c
@@ -756,7 +756,7 @@ check_natural_geometry(struct view *view)
 	if (!view_is_floating(view)
 			&& (view->natural_geometry.width < LAB_MIN_VIEW_WIDTH
 			|| view->natural_geometry.height < LAB_MIN_VIEW_HEIGHT)) {
-		view_set_fallback_natural_geometry(view);
+		view->natural_geometry = view_get_fallback_natural_geometry(view);
 	}
 }
 

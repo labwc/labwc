labwc/labwc

commit 3f223fe5b08c2ae780cb703fbc16ebf487e5fa69

xdg: unify initial positioning logic

src/xdg.c
@@ -121,17 +121,32 @@ set_fullscreen_from_request(struct view *view,
 	view_set_fullscreen(view, requested->fullscreen);
 }
 
+/* Called from commit handler and updates view->pending.x/y directly */
 static void
-do_late_positioning(struct view *view)
+set_initial_position(struct view *view)
 {
+	if (!view_is_floating(view)) {
+		return;
+	}
+
+	view_constrain_size_to_that_of_usable_area(view);
+
 	struct server *server = view->server;
 	if (server->input_mode == LAB_INPUT_STATE_MOVE
 			&& view == server->grabbed_view) {
 		/* Reposition the view while anchoring it to cursor */
 		interactive_anchor_to_cursor(server, &view->pending);
 	} else {
-		view_compute_position_by_policy(view, &view->pending,
-			/* allow_cursor */ true, rc.placement_policy);
+		struct view *parent = xdg_toplevel_view_get_parent(view);
+		if (parent) {
+			/* Center relative to parent view */
+			view_compute_centered_position(view, &parent->pending,
+				view->pending.width, view->pending.height,
+				&view->pending.x, &view->pending.y);
+		} else {
+			view_compute_position_by_policy(view, &view->pending,
+				/* allow_cursor */ true, rc.placement_policy);
+		}
 	}
 }
 
@@ -247,15 +262,15 @@ handle_commit(struct wl_listener *listener, void *data)
 	bool update_required = false;
 
 	/*
-	 * If we didn't know the natural size when leaving fullscreen or
-	 * unmaximizing, then the pending size will be 0x0. In this case,
-	 * the pending x/y is also unset and we still need to position
-	 * the window.
+	 * The pending size will be empty in two cases:
+	 *   (1) when the view is first mapped
+	 *   (2) when leaving fullscreen or un-maximizing,
+	 *       if natural geometry wasn't known
 	 */
 	if (wlr_box_empty(&view->pending) && !wlr_box_empty(&size)) {
 		view->pending.width = size.width;
 		view->pending.height = size.height;
-		do_late_positioning(view);
+		set_initial_position(view);
 		update_required = true;
 	}
 
@@ -790,22 +805,6 @@ xdg_toplevel_view_notify_tiled(struct view *view)
 }
 
 static void
-set_initial_position(struct view *view)
-{
-	view_constrain_size_to_that_of_usable_area(view);
-
-	struct view *parent = xdg_toplevel_view_get_parent(view);
-	if (parent) {
-		/* Child views are center-aligned relative to their parents */
-		view_center(view, &parent->pending);
-		return;
-	}
-
-	/* All other views are placed according to a configured strategy */
-	view_place_by_policy(view, /* allow_cursor */ true, rc.placement_policy);
-}
-
-static void
 handle_map(struct wl_listener *listener, void *data)
 {
 	struct view *view = wl_container_of(listener, view, mappable.map);
@@ -821,36 +820,6 @@ handle_map(struct wl_listener *listener, void *data)
 		} else {
 			view_set_ssd_mode(view, LAB_SSD_MODE_NONE);
 		}
-
-		/*
-		 * Set initial "pending" dimensions. "Current"
-		 * dimensions remain zero until handle_commit().
-		 */
-		if (wlr_box_empty(&view->pending)) {
-			struct wlr_xdg_surface *xdg_surface =
-				xdg_surface_from_view(view);
-			view->pending.width = xdg_surface->geometry.width;
-			view->pending.height = xdg_surface->geometry.height;
-		}
-
-		/*
-		 * Set initial "pending" position for floating views.
-		 */
-		if (view_is_floating(view)) {
-			set_initial_position(view);
-		}
-
-		/* Disable background fill at map (paranoid?) */
-		disable_fullscreen_bg(view);
-
-		/*
-		 * Set initial "current" position directly before
-		 * calling view_moved() to reduce flicker
-		 */
-		view->current.x = view->pending.x;
-		view->current.y = view->pending.y;
-
-		view_moved(view);
 	}
 
 	view_impl_map(view);

commit 8039c7e22b0d33f4ec3937db545333d6c027ec68

view: save `view->last_placement` on initial positioning
Fix a regression introduced in 3f223fe5 where output layout changes may
move xdg views outside of the output layout with the error:
`view has no last placement info`. This happens also for X11 views if
they don't specify its own geometry on map.

Recent changes introduced `view->last_placement` which is used to
restore view positions after layout changes. This is supposed to be
updated when a view is mapped or manually moved by the user.

However, 3f223fe5 replaced `view_place_by_policy()` with
`view_compute_position_by_policy()` in the xdg view map handler. Unlike
`view_place_by_policy()`, this function does not update
`view->last_placement`, leaving it unset at the time of output layout
changes.

Therefore, this patch adds explicit calls to `view_save_last_placement()`
in the map handler to ensure `view->last_placement` is always set for
mapped views.

include/view.h
@@ -564,7 +564,15 @@ bool view_titlebar_visible(struct view *view);
 void view_set_ssd_mode(struct view *view, enum lab_ssd_mode mode);
 void view_set_decorations(struct view *view, enum lab_ssd_mode mode, bool force_ssd);
 void view_toggle_fullscreen(struct view *view);
+
+/*
+ * Saves the window position in view->last_placement. This should be called
+ * when a view is first mapped or manually moved by the user.
+ */
+void view_save_last_placement(struct view *view);
+/* Restores and adjusts the view's position from the view->last_placement */
 void view_adjust_for_layout_change(struct view *view);
+
 void view_move_to_edge(struct view *view, enum lab_edge direction, bool snap_to_windows);
 void view_grow_to_edge(struct view *view, enum lab_edge direction);
 void view_shrink_to_edge(struct view *view, enum lab_edge direction);

src/view.c
@@ -568,8 +568,6 @@ view_moved(struct view *view)
 	}
 }
 
-static void save_last_placement(struct view *view);
-
 void
 view_move_resize(struct view *view, struct wlr_box geo)
 {
@@ -589,7 +587,7 @@ view_move_resize(struct view *view, struct wlr_box geo)
 	 * Not sure if it might have other side-effects though.
 	 */
 	if (!view->adjusting_for_layout_change) {
-		save_last_placement(view);
+		view_save_last_placement(view);
 	}
 }
 
@@ -1722,8 +1720,8 @@ view_set_fullscreen(struct view *view, bool fullscreen)
 	cursor_update_focus(view->server);
 }
 
-static void
-save_last_placement(struct view *view)
+void
+view_save_last_placement(struct view *view)
 {
 	assert(view);
 	struct output *output = view->output;

src/xdg.c
@@ -148,6 +148,8 @@ set_initial_position(struct view *view)
 				/* allow_cursor */ true, rc.placement_policy);
 		}
 	}
+
+	view_save_last_placement(view);
 }
 
 static void

src/xwayland.c
@@ -786,6 +786,9 @@ set_initial_position(struct view *view,
 		}
 	}
 
+	/* view->last_placement is still unset if has_position=true */
+	view_save_last_placement(view);
+
 	/*
 	 * Always make sure the view is onscreen and adjusted for any
 	 * layout changes that could have occurred between map_request
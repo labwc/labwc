dfc3239ced4790f740b728fc06064154bcc0e6bc
Jack Zeal <hakfoo@gmail.com>
2026-04-04 00:02:10 -0700

    Remove duplicate re-rendering:

--- src/ssd/ssd.c
+++ src/ssd/ssd.c
@@ -176,7 +176,6 @@ ssd_create(struct view *view, bool active)
 	ssd_enable_keybind_inhibit_indicator(ssd, view->inhibits_keybinds);
 	ssd->state.geometry = view->current;
 
-	ssd_titlebar_update(ssd);
 	return ssd;
 }
 

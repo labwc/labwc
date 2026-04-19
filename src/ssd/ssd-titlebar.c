4570bdbade44d4070d6287da2318f8a339fc9c82
chocolatemintychip <165883340+chocolatemintychip@users.noreply.github.com>
2026-04-20 01:33:20 +0200

    ssd: remove a temp printf

--- src/ssd/ssd-titlebar.c
+++ src/ssd/ssd-titlebar.c
@@ -299,8 +299,6 @@ ssd_titlebar_update(struct ssd *ssd)
 		ssd->state.was_omnipresent = view->visible_on_all_workspaces;
 	}
 
-	printf("%d-%d-%d\n",ssd->state.was_ontop, view->layer,VIEW_LAYER_ALWAYS_ON_TOP);
-
 	if (ssd->state.was_ontop != (view->layer == VIEW_LAYER_ALWAYS_ON_TOP)) {
 		set_alt_button_icon(ssd, LAB_NODE_BUTTON_ONTOP,
 			view->layer);

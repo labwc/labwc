311294f4425174d3852333a320c4882fe66a8aed
elviosak <33790211+elviosak@users.noreply.github.com>
2026-07-16 18:31:11 -0300

    nag_log: add logger for config errors
    Also create <core><errorCommand> to choose the client to display it.

---
+++ include/common/nag.h
@@ -0,0 +1,22 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+#ifndef LABWC_NAG_H
+#define LABWC_NAG_H
+
+#include <sys/wait.h>
+#include <wlr/util/log.h>
+#include "common/buf.h"
+
+struct buf *nag_get_buf(void);
+void nag_set_error(enum wlr_log_importance importance);
+void nag_check_pid(pid_t exited_pid);
+void nag_reset(void);
+void nag_show_callback(void *data);
+
+#define nag_log(verbosity, fmt, ...) \
+do { \
+	wlr_log(verbosity, fmt, ##__VA_ARGS__); \
+	nag_set_error(verbosity); \
+	buf_add_fmt(nag_get_buf(), fmt "\n", ##__VA_ARGS__); \
+} while (0)
+
+#endif /* LABWC_NAG_H */

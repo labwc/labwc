661e1f2e124152b44ccabe04d7a4892996c2f8b9
elviosak <33790211+elviosak@users.noreply.github.com>
2026-07-21 17:57:16 -0300

    add handle_writable() to handle cases where write() doesn't write the entire message.

--- src/common/nag.c
+++ src/common/nag.c
@@ -1,15 +1,20 @@
 // SPDX-License-Identifier: GPL-2.0-only
 #define _POSIX_C_SOURCE 200809L
 #include "common/nag.h"
+#include <assert.h>
 #include <stdarg.h>
 #include <unistd.h>
 #include "common/buf.h"
 #include "config/rcxml.h"
 #include "common/spawn.h"
+#include "labwc.h"
 
 static struct buf log_buf = BUF_INIT;
 static bool has_error = false;
 static pid_t pid = 0;
+static int pipe_w;
+static struct wl_event_source *write_notifier = NULL;
+static size_t remaining = 0;
 
 struct buf *
 nag_get_buf(void)
@@ -45,21 +50,41 @@ nag_reset(void)
 	pid = 0;
 }
 
+static int
+handle_writable(int fd, uint32_t mask, void *data)
+{
+	assert(remaining > 0);
+	wlr_log(WLR_ERROR, "Writable");
+	ssize_t bytes = write(pipe_w, log_buf.data + (log_buf.len - remaining), remaining);
+	if (bytes < 0) {
+		wlr_log(WLR_ERROR, "Failed to write errors to process: %s", rc.error_command);
+	} else {
+		remaining -= bytes;
+		if (remaining > 0) {
+			goto keep_waiting;
+		}
+	}
+
+	wl_event_source_remove(write_notifier);
+	write_notifier = NULL;
+	spawn_piped_close(pid, pipe_w);
+
+keep_waiting:
+	return 0;
+}
+
 static void
 nag_show(void)
 {
 	if (!has_error) {
 		return;
 	}
-	int pipe_w;
 	pid = spawn_piped_async_no_shell(rc.error_command, &pipe_w);
 	if (pid > 0) {
-		ssize_t bytes = write(pipe_w, log_buf.data, log_buf.len);
-		if (bytes < 0) {
-			wlr_log(WLR_ERROR, "Failed to write errors to process: %s",
-				rc.error_command);
-		}
-		spawn_piped_close(pid, pipe_w);
+		assert(!write_notifier);
+		remaining = log_buf.len;
+		write_notifier = wl_event_loop_add_fd(server.wl_event_loop,
+			pipe_w, WL_EVENT_WRITABLE, handle_writable, NULL);
 	} else if (pid < 0) {
 		wlr_log(WLR_ERROR, "Failed to launch process: %s", rc.error_command);
 	}

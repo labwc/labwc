e31184e6602c85d5808c414b2a2d87be357daac7
Johan Malm <jgm323@gmail.com>
2026-09-15 19:51:48 +0100

    string-helpers.c: cast isspace() input to unsigned char
    ...as a defensive step to avoid undefined behaviour with a character like
    é (e.g. in café). In UTF-8, the é character occupies two bytes, 0xC3 and
    0xA9, which as (signed) chars become the negative values -61 and -87
    respectively.
    
    The POSIX standard requires that the argument for isspace() is either EOF
    or a value that is representable in the type unsigned char; otherwise, the
    behavior is undefined. The isalpha(3) man page says that if the argument
    is of type char, it must be  cast to unsigned char.

--- src/common/string-helpers.c
+++ src/common/string-helpers.c
@@ -28,7 +28,7 @@ rtrim(char *s)
 		return;
 	}
 	char *end = s + len - 1;
-	while (end >= s && isspace(*end)) {
+	while (end >= s && isspace((unsigned char)*end)) {
 		end--;
 	}
 	*(end + 1) = '\0';
@@ -37,7 +37,7 @@ rtrim(char *s)
 char *
 string_strip(char *s)
 {
-	while (isspace(*s)) {
+	while (isspace((unsigned char)*s)) {
 		s++;
 	}
 	rtrim(s);
@@ -202,7 +202,7 @@ bool
 str_space_only(const char *s)
 {
 	for (; *s; s++) {
-		if (!isspace(*s)) {
+		if (!isspace((unsigned char)*s)) {
 			return false;
 		}
 	}
